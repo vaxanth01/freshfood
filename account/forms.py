4ab04ea154ce724d7f47c24e2a92139615e99789
vasanthkumar <150125632+vaxanth01@users.noreply.github.com>
2024-12-30 11:07:01 +0530

    done e com

---
+++ account/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.forms import UserCreationForm 
+from django.contrib.auth.models import User
+from django import forms
+
+# Form details for user while creating
+class MyUserCreationForm(UserCreationForm):
+
+  username=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter User Name'}))
+  email=forms.CharField(widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Enter Email Address'}))
+  password1=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Enter Your Password'}))
+  password2=forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Enter Confirm Password'}))
+  class Meta:
+        model = User
+        fields = ['username', 'email' , 'password1', 'password2',]
+
+        
+
+
+
